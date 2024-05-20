c2333f2d5d84a1ce59cbce68a9a8caa188609d47
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-05-21 01:36:17 +1000

    add text

--- src/pages/DiseaseTarget.jsx
+++ src/pages/DiseaseTarget.jsx
@@ -10,20 +10,25 @@ export default function DiseaseTarget() {
       <Breadcrumbs />
       <div className='grid grid-cols-2 py-4 px-16'>
         <div className='grid-1 bg-white py-4 px-8'>
-          <h2 className='text-2xl font-bold text-center py-2'>
+          <h2 className='text-2xl font-bold text-center pt-2 pb-4'>
             About Alzheimer’s Disease
           </h2>
-          <p>
-            Alzheimer's disease is a progressive disorder that causes brain cells to waste away (degenerate) and die. Alzheimer's disease is the most common cause of dementia — a continuous decline in thinking, behavioral and social skills that disrupts a person's ability to function independently.
+          <p className='leading-7'>
+          Alzheimer's disease (AD) is a chronic brain damage disease associated with inflammation in the brain, leading to symptoms such as memory loss, difficulty concentrating and changes in behaviour.<br/><br/>
+          In Australia, AD affects up to 1 in 10 Australians over 65 years of age, going up to 3 in 10 Australians over 85 years old. Almost two-thirds of those affected are female patients. (Data source: )<br/><br/>
+          The exact causes of AD are still not fully understood, but age is the most significant risk factor, along with a combination of genetic, lifestyle and environmental factors. The pathogenesis of AD is due to amyloid beta (Aβ) and neurofibrillary tangles (NFTs) abnormal phosphorylated (unstable) of tau protein in the hippocampus, causing toxic effects on neurons and breaking the communication between brain cells, resulting in brain damage with memory loss and other symptoms.<br/><br/>
+          Recent common treatments of AD are based on cholinesterase inhibitors which can reconnect cell-to-cell communication levels.3 However, it cannot completely cure the destruction of neuron connection, so this treatment only can be a pathway for delaying the AD process. Hence, a promising is in finding a novel target for treating AD entirely.<br/><br/>
           </p>
         </div>
 
         <div className='grid-2 bg-white py-4 px-8'>
-          <h2 className='text-2xl font-bold text-center py-2'>
+          <h2 className='text-2xl font-bold text-center pt-2 pb-4'>
             About the Novel Target – HSP70
           </h2>
-          <p>
-            The identification of a target is the first step in the drug discovery process. A target is a molecule or structure in the body that the drug is designed to interact with. The target is usually a protein that is involved in a disease process. The drug is designed to interact with the target in a way that stops or slows down the disease process.
+          <p className='leading-7'>
+          The novel target is called Heat Shock Proteins 70 (HSP70), a molecular chaperone, which can maintain protein homeostasis by controlling brain protein expression by activating HSP70, recovering the neurons and tau protein structure. <br/><br/>
+          Activating the HSP70 protein would promote tau binding to microtubules, and inhibit the self-assembly of Aβ, which increases the stability of the structure.<br/><br/>
+          An expected outcome in the lab, activating HSP70 can regain homeostasis and possibly reversal by enhancing the tau stabilisation and regulation to treat the disease.
           </p>
         </div>
       </div>
--- src/pages/HitDiscoveryOptimization.jsx
+++ src/pages/HitDiscoveryOptimization.jsx
@@ -10,16 +10,19 @@ export default function HitDiscoveryOptimization() {
       </div>
       <Breadcrumbs />
       <div className='grid grid-cols-2 gap-4 py-4 px-24'>
-        <div className='grid-1 bg-white p-4'>
-          <img src={MedicinePic} alt='medicine' className='h-80 ml-20' />
+        <div className='grid-1 bg-white p-4 my-auto pl-16'>
+          <img src={MedicinePic} alt='medicine' className='h-80 flex flex-col justify-center items-center' />
+          <p className='text-gray-500 text-left mt-4'>Figure. 115-7c Optimization Compound</p>
         </div>
 
         <div className='grid-2 bg-white p-4'>
           <h2 className='text-2xl font-bold text-center py-2'>
             115-7c Optimization Compound
           </h2>
-          <p>
-            The identification of a target is the first step in the drug discovery process. A target is a molecule or structure in the body that the drug is designed to interact with. The target is usually a protein that is involved in a disease process. The drug is designed to interact with the target in a way that stops or slows down the disease process.
+          <p className='leading-7'>
+          Using high-throughput screening (HTS), one of the most common techniques in the drug discovery process, with screening and identifying potential hits for HSP70 activators in HeLa cells from specific compound libraries (database: PubChem AID 1203) with 196,225 tested, resulting in potential activators for HSP70 (ML346 and 115-7c).<br/><br/>
+          After comparing the physiochemical/drug-like properties for ML346 and 115-7c, the final choice for optimised hit is 115-7c.<br/><br/>
+          The modification process of 115-7c is replacing dichlorobenzene with methyl, changing the carboxyl group to methyl, and attaching methyl to the top of a nitrogen atom that is not attached to a hydrogen atom. Compared to the parent compound, our new compound shows better drug-like properties with lower molecular weight, better log P value, the ability to cross the brain-blood-barrier (BBB) and obey the Lipinski RO5, leading to a promising oral tablet for treating Alzheimer’s Disease.<br/><br/>
           </p>
         </div>
       </div>
