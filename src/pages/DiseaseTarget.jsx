ddb14efddd81814de8777d7784af9204a6049b79
Oliver Wu <z5379238@ad.unsw.edu.au>
2024-05-30 22:25:40 +1000

    final content added

--- src/pages/DiseaseTarget.jsx
+++ src/pages/DiseaseTarget.jsx
@@ -14,10 +14,20 @@ export default function DiseaseTarget() {
             About Alzheimer’s Disease
           </h2>
           <p className='leading-7'>
-          Alzheimer's disease (AD) is a chronic brain damage disease associated with inflammation in the brain, leading to symptoms such as memory loss, difficulty concentrating and changes in behaviour.<br/><br/>
-          In Australia, AD affects up to 1 in 10 Australians over 65 years of age, going up to 3 in 10 Australians over 85 years old. Almost two-thirds of those affected are female patients. (Data source: )<br/><br/>
-          The exact causes of AD are still not fully understood, but age is the most significant risk factor, along with a combination of genetic, lifestyle and environmental factors. The pathogenesis of AD is due to amyloid beta (Aβ) and neurofibrillary tangles (NFTs) abnormal phosphorylated (unstable) of tau protein in the hippocampus, causing toxic effects on neurons and breaking the communication between brain cells, resulting in brain damage with memory loss and other symptoms.<br/><br/>
-          Recent common treatments of AD are based on cholinesterase inhibitors which can reconnect cell-to-cell communication levels.3 However, it cannot completely cure the destruction of neuron connection, so this treatment only can be a pathway for delaying the AD process. Hence, a promising is in finding a novel target for treating AD entirely.<br/><br/>
+          Alzheimer's disease (AD) is a chronic brain damage disease associated with inflammation in the brain, leading to symptoms such as memory loss, difficulty concentrating and changes in behaviour.
+          <a href='https://pubmed.ncbi.nlm.nih.gov/31410002/' target="_blank"><sup>1</sup></a>
+          <br/><br/>
+          In Australia, AD affects up to 1 in 10 Australians over 65 years of age, going up to 3 in 10 Australians over 85 years old. Almost two-thirds of those affected are female patients.
+          <a href='https://www.aihw.gov.au/reports/dementia/dementia-in-aus/contents/about' target="_blank"><sup>2</sup></a>
+          <br/><br/>
+          The exact causes of AD are still not fully understood, but age is the most significant risk factor, along with a combination of genetic, lifestyle and environmental factors. The pathogenesis of AD is due to amyloid beta (Aβ) and neurofibrillary tangles (NFTs) abnormal phosphorylated (unstable) of tau protein in the hippocampus, causing toxic effects on neurons and breaking the communication between brain cells, resulting in brain damage with memory loss and other symptoms.
+          <a href='https://pubmed.ncbi.nlm.nih.gov/31410002/' target="_blank"><sup>1</sup></a>
+          <br/><br/>
+          Recent common treatments of AD are based on cholinesterase inhibitors which can reconnect cell-to-cell communication levels.&nbsp;
+          <a href='https://www.nejm.org/doi/full/10.1056/NEJM199911253412207' target="_blank"><sup>3</sup></a>
+          However, it cannot completely cure the destruction of neuron connection, so this treatment only can be a pathway for delaying the AD process. Hence, a promising is in finding a novel target for treating AD entirely.
+          <a href='https://www.mayoclinic.org/diseases-conditions/alzheimers-disease/diagnosis-treatment/drc-20350453' target="_blank"><sup>4</sup></a>
+          <br/><br/>
           </p>
         </div>
 
@@ -26,12 +36,32 @@ export default function DiseaseTarget() {
             About the Novel Target – HSP70
           </h2>
           <p className='leading-7'>
-          The novel target is called Heat Shock Proteins 70 (HSP70), a molecular chaperone, which can maintain protein homeostasis by controlling brain protein expression by activating HSP70, recovering the neurons and tau protein structure. <br/><br/>
-          Activating the HSP70 protein would promote tau binding to microtubules, and inhibit the self-assembly of Aβ, which increases the stability of the structure.<br/><br/>
+          The novel target is called Heat Shock Proteins 70 (HSP70), a molecular chaperone, which can maintain protein homeostasis by controlling brain protein expression by activating HSP70, recovering the neurons and tau protein structure. 
+          <a href='https://link.springer.com/article/10.1007/s11010-013-1844-y' target="_blank"><sup>5</sup></a>
+          <br/><br/>
+          Activating the HSP70 protein would promote tau binding to microtubules, and inhibit the self-assembly of Aβ, which increases the stability of the structure.
+          <a href='https://pubs.acs.org/doi/10.1021/jm100054f' target="_blank"><sup>6</sup></a>
+          <br/><br/>
           An expected outcome in the lab, activating HSP70 can regain homeostasis and possibly reversal by enhancing the tau stabilisation and regulation to treat the disease.
+          <a href='https://www.ncbi.nlm.nih.gov/books/NBK148494/' target="_blank"><sup>7</sup></a>
           </p>
         </div>
       </div>
+
+      <div className='bg-white py-8 flex flex-col justify-center items-center'>
+            <h2 className='text-2xl font-heading2 tracking-wide font-bold text-center py-2'>
+              References
+            </h2>
+            <ol className='list-decimal list-inside'>
+              <li><a href='https://pubmed.ncbi.nlm.nih.gov/31410002/' target="_blank" className='text-gray-500 hover:underline'>https://pubmed.ncbi.nlm.nih.gov/31410002/</a></li>
+              <li><a href='https://www.aihw.gov.au/reports/dementia/dementia-in-aus/contents/about' target="_blank" className='text-gray-500 hover:underline'>https://www.aihw.gov.au/reports/dementia/dementia-in-aus/contents/about</a></li>
+              <li><a href='https://www.nejm.org/doi/full/10.1056/NEJM199911253412207' target="_blank" className='text-gray-500 hover:underline'>https://www.nejm.org/doi/full/10.1056/NEJM199911253412207</a></li>
+              <li><a href='https://www.mayoclinic.org/diseases-conditions/alzheimers-disease/diagnosis-treatment/drc-20350453' target="_blank" className='text-gray-500 hover:underline'>https://www.mayoclinic.org/diseases-conditions/alzheimers-disease/diagnosis-treatment/drc-20350453</a></li>
+              <li><a href='https://link.springer.com/article/10.1007/s11010-013-1844-y' target="_blank" className='text-gray-500 hover:underline'>https://link.springer.com/article/10.1007/s11010-013-1844-y</a></li>
+              <li><a href='https://pubs.acs.org/doi/10.1021/jm100054f' target="_blank" className='text-gray-500 hover:underline'>https://pubs.acs.org/doi/10.1021/jm100054f</a></li>
+              <li><a href='https://www.ncbi.nlm.nih.gov/books/NBK148494/' target="_blank" className='text-gray-500 hover:underline'>https://www.ncbi.nlm.nih.gov/books/NBK148494/</a></li>
+            </ol>
+          </div>
     </main>
   )
 }
--- src/pages/HitDiscoveryOptimization.jsx
+++ src/pages/HitDiscoveryOptimization.jsx
@@ -6,7 +6,7 @@ export default function HitDiscoveryOptimization() {
   return (
     <main>
       <div className='bg-regal-purple font-heading2 tracking-wide px-4 py-2 text-white flex items-center justify-center text-2xl h-16'>
-      Hit Discovery & Optimization
+        Hit Discovery & Optimization
       </div>
       <Breadcrumbs />
       <div className='grid grid-cols-2 gap-4 py-4 px-24'>
@@ -20,12 +20,122 @@ export default function HitDiscoveryOptimization() {
             115-7c Optimization Compound
           </h2>
           <p className='leading-7'>
-          Using high-throughput screening (HTS), one of the most common techniques in the drug discovery process, with screening and identifying potential hits for HSP70 activators in HeLa cells from specific compound libraries (database: PubChem AID 1203) with 196,225 tested, resulting in potential activators for HSP70 (ML346 and 115-7c).<br/><br/>
-          After comparing the physiochemical/drug-like properties for ML346 and 115-7c, the final choice for optimised hit is 115-7c.<br/><br/>
-          The modification process of 115-7c is replacing dichlorobenzene with methyl, changing the carboxyl group to methyl, and attaching methyl to the top of a nitrogen atom that is not attached to a hydrogen atom. Compared to the parent compound, our new compound shows better drug-like properties with lower molecular weight, better log P value, the ability to cross the brain-blood-barrier (BBB) and obey the Lipinski RO5, leading to a promising oral tablet for treating Alzheimer’s Disease.<br/><br/>
+          Using high-throughput screening (HTS), one of the most common techniques in the drug discovery process
+          <a href='https://www.mdpi.com/1422-0067/13/1/427' target="_blank"><sup>1</sup></a>
+          , with screening and identifying potential hits for HSP70 activators in HeLa cells from specific compound libraries (database: PubChem AID 1203) with 196,225 tested, resulting in potential activators for HSP70 (ML346 and 115-7c).
+          <a href='https://www.ncbi.nlm.nih.gov/books/NBK148494/' target="_blank"><sup>2</sup></a><br/><br/>
+          After comparing the physiochemical/drug-like properties for ML346 and 115-7c, the final choice for optimised hit is 115-7c. (Table 1) (Results were generated from&nbsp;
+          <a href='http://www.swissadme.ch/index.php' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600' target='_blank'>SwisseADME</a>
+          )<br/><br/>
+            The modification process of 115-7c is replacing dichlorobenzene with methyl, changing the carboxyl group to methyl, and attaching methyl to the top of a nitrogen atom that is not attached to a hydrogen atom. Compared to the parent compound, our new compound shows better drug-like properties with lower molecular weight, better log P value, the ability to cross the brain-blood-barrier (BBB) and obey the Lipinski RO5, leading to a promising oral tablet for treating Alzheimer’s Disease.<br/><br/>
           </p>
         </div>
       </div>
+
+      <div className='overflow-x-auto'>
+        <h2 className='font-heading2 tracking-wide text-center py-2'>
+          Table 1. The physiochemical/drug-like properties for ML346, 115-7c and 115-7c optimisation(Cognivita<sup>©</sup>).
+          </h2>
+            <table className='mx-auto bg-white'>
+              <thead>
+                <tr>
+                  <th className='py-2 px-4 border-b border-gray-200 bg-gray-100'>Property</th>
+                  <th className='py-2 px-4 border-b border-gray-200 bg-gray-100'>ML346</th>
+                  <th className='py-2 px-4 border-b border-gray-200 bg-gray-100'>115-7c</th>
+                  <th className='py-2 px-4 border-b border-gray-200 bg-gray-100'>115-7c Optimisation</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Molecular Weight</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>272.26 g/mol</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>477.34 g/mol</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>330.42 g/mol</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Log P</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>1.62</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>3.14</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>3.44</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Molar Refractivity</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>78.89</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>128.57</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>101.91</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Total Number of Atoms</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>32</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>54</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>50</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Number of H-bond Donors</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>2</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>2</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>1</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Number of H-bond Acceptors</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>4</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>5</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>3</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Number of Rotatable Bonds</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>3</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>9</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>7</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Number of Rings</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>2</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>3</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>2</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Total PSA</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>84.50 Å<sup>2</sup></td>
+                  <td className='py-2 px-4 border-b border-gray-200'>95.94 Å<sup>2</sup></td>
+                  <td className='py-2 px-4 border-b border-gray-200'>58.64 Å<sup>2</sup></td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Bioavailability</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>0.55</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>0.56</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>0.55</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Water Solubility</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>Soluble</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>Moderately Soluble</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>Soluble</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>GI Absorption</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>High</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>High</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>High</td>
+                </tr>
+                <tr>
+                  <td className='py-2 px-4 border-b border-gray-200'>Blood Brain Barrier (BBB) Permeable</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>No</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>No</td>
+                  <td className='py-2 px-4 border-b border-gray-200'>Yes</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='bg-white py-8 flex flex-col justify-center items-center'>
+            <h2 className='text-2xl font-heading2 tracking-wide font-bold text-center py-2'>
+              References
+            </h2>
+            <ol className='list-decimal list-inside'>
+              <li><a href='https://www.mdpi.com/1422-0067/13/1/427' target="_blank" className='text-gray-500 hover:underline'>https://www.mdpi.com/1422-0067/13/1/427</a></li>
+              <li><a href='https://www.ncbi.nlm.nih.gov/books/NBK148494/' target="_blank" className='text-gray-500 hover:underline'>https://www.ncbi.nlm.nih.gov/books/NBK148494/</a></li>
+            </ol>
+          </div>
     </main>
   )
 }
